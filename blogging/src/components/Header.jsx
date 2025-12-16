a35e2326a9582599bc8ff2dab31f5734c1c56c69
iabbas96 <ibrahimabbas970@gmail.com>
2025-12-16 16:22:30 +0100

    fix: Added outline to the sign in button

--- blogging/src/components/Header.jsx
+++ blogging/src/components/Header.jsx
@@ -30,13 +30,13 @@ export default function Header() {
                 </Button>
 
                 <Link to="/sign-in">
-                    <Button gradientDuoTone='purpleToBlue'>Sign in</Button>
+                    <Button gradientDuoTone='purpleToBlue' outline>Sign in</Button>
                 </Link>
                 <Navbar.Toggle />
            </div>
                 {/* collpse mobile view */}
                 <Navbar.Collapse>
-                    <Navbar.Link active={path === "/"} as={'div'}>
+                    <Navbar.Link active={path === "/" } as={'div'}>
                         <Link to="/">Home</Link>
                     </Navbar.Link>
                     <Navbar.Link active={path === "/about"} as={'div'}>
